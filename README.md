ac135fc52964f80a6fa3b632b408fa67e6c018ef
John Lampe <jlampe@tns4361l.local>
2018-04-10 21:57:49 -0400

    cleanup

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # NNM-IR
-Incident Response (IR) plugins that detect stuff that I'm interested in detecting :) You'll need Nessus Network Monitor (NNM...formerly known as PVS) to use these:
+Incident Response (IR) plugins that detect stuff that I'm interested in detecting :) You'll need Nessus Network Monitor (NNM...formerly known as PVS) to use these.
 
-run sh ./install_files.sh 
+To install:
+sudo ./install_files.sh 
+
 
--- install_files.sh
+++ install_files.sh
@@ -3,6 +3,7 @@
 script_dir=/opt/pvs/var/pvs/scripts/
 plugin_dir=/opt/pvs/var/pvs/plugins/
 bin_location=/opt/pvs/bin/pvs-proxy
+pvs_binary=/opt/pvs/bin/pvs
 
 if [ ! -d $script_dir ]
 then echo "Could not stat $script_dir . Exiting"
@@ -19,13 +20,21 @@ then echo "Could not stat $bin_location . Exiting"
 exit
 fi
 
+if [ ! -f $pvs_binary ]
+then echo "Could not stat $pvs_binary . Exiting"
+exit
+fi
 
-sudo ps auxww | awk '/pvs/ {print "kill -9 " $2}' | sh
+# Ensure realtime logging is enabled
+$pvs_binary --config "Log Realtime Events To Realtime Log File" 1
 
-sudo cp *.inc $script_dir 
-sudo cp *.pasl $script_dir
-sudo cp *.prm $plugin_dir
+# kill all running pvs processes
+ps auxww | awk '/pvs/ {print "kill -9 " $2}' | sh
 
-sudo $bin_location & 
+cp *.inc $script_dir 
+cp *.pasl $script_dir
+cp *.prm $plugin_dir
+
+$bin_location & 
 
 
